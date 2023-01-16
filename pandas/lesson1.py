7b6f318f01e0dc259839b876f4d8e89757918ba5
Alex <a.nicolaidis@outlook.com>
2023-01-16 17:09:12 +1000

    Started pandas exercises

--- pandas/lesson1.py
+++ pandas/lesson1.py
@@ -2,3 +2,21 @@
 
 import pandas as pd 
 
+#1
+
+fruits = pd.DataFrame({"Apples": [30], "Bananas": [21]})
+
+print(fruits)
+
+
+#2
+
+fruit_sales = pd.DataFrame({"Apples": [35,41], "Bananas":[21,34]}, index = ["2017 Sales", "2018 Sales"])
+
+print(fruit_sales)
+
+#3 
+
+ingredients = pd.Series(["4 cups", "1 cup", "2 large", "1 can"], index = ['Flour', 'Milk', "Eggs", "Spam",], name = "Dinner")
+
+print(ingredients)
